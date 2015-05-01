bab4e594d57af2412273e1a4553d11292022088a
mumelmann <mumelmann@gmail.com>
2015-05-01 12:02:57 +0200

    separate urls file for lists.

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from lists import views as lists_views
+from lists import urls as lists_urls
+
 
 urlpatterns = [
     # Examples:
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    url(r'^lists/(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^$', lists_views.home_page, name='home'),
+    url(r'^lists/', include(lists_urls)),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 ]
